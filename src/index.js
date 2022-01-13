dcdf0d78d2ffe050d12e092317138ed0e3c9b500
Camusat Antoine <antoine.camusat@insa-rouen.fr>
2022-01-13 20:04:45 +0100

    proper demo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const ffi = require('ffi-napi');
 const bodyParser = require('body-parser');
 
 const app = express()
-const port = 3000
+const port = 8080
 
 //Serve public folder
 app.use(express.static('public'));
@@ -18,7 +18,7 @@ app.use(bodyParser.raw({
 
 //Define the library API (see r2ddoc.h)
 //http://jakegoulding.com/rust-ffi-omnibus/string_return/
-var libr2ddoc = ffi.Library('lib/libr2ddoc.so', {
+const libr2ddoc = ffi.Library('lib/libr2ddoc.so', {
     "lib2ddoc_data_to_json": ['char *', ['char *', 'int']],
     "lib2ddoc_image_to_json": ['char *', ['char *', 'int', 'int', 'int']],
     "lib2ddoc_free_json": ['void', ['char *']]
@@ -28,11 +28,12 @@ var libr2ddoc = ffi.Library('lib/libr2ddoc.so', {
 app.post('/decode', (req, res) => {
     console.log(`[${req.ip}] Start processing...`);
 
-    let base64 = req.body.toString().replace(/^data:image\/png;base64,/, "");
-    let png = PNG.sync.read(new Buffer.from(base64, 'base64'));
+    const base64 = req.body.toString().replace(/^data:image\/png;base64,/, "");
+    const png = PNG.sync.read(new Buffer.from(base64, 'base64'));
 
-    let jsonPtr = libr2ddoc.lib2ddoc_image_to_json(png.data, png.width, png.height, 600);
-    let json = jsonPtr.readCString();
+    const jsonPtr = libr2ddoc.lib2ddoc_image_to_json(png.data, png.width, png.height, 600);
+    const json = jsonPtr.readCString();
+    
     libr2ddoc.lib2ddoc_free_json(jsonPtr);
 
     res.send(json);
